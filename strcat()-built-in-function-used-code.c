9857728706141fbde8d3b86a5ee0f4ad247ef485
Isha Negi <itsishanegi@gmail.com>
2024-09-23 07:09:13 +0530

    Add files via upload

---
+++ strcat()-built-in-function-used-code.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+#include<string.h>
+
+void main()
+
+{ char str1[50]="hello, ";
+ 
+char str2[]="world!";
+
+ strcat(str1,str2);
+ 
+printf("concatenated string: %s",str1);
+
+}
